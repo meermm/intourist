79941dca17cc46d991efd6f1f5b24386b37fc07a
MeerimI <irismamatovameerim@gmail.com>
2021-07-06 16:17:06 +0600

    add forms, crud based on fbv

--- core/views.py
+++ core/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 
 def homepage(request):
-    return render(request, 'index.html')
+    return render(request, 'core/index.html')
 
 
 def profile(request):
     user = request.user
     profile_object = user.profile
-    return render(request, 'profile.html', {
+    return render(request, 'core/profile.html', {
         'profile':profile_object,
     })
---
+++ places/forms.py
@@ -0,0 +1,8 @@
+from django.db.models import fields
+from django import forms
+from .models import Place
+
+class PlaceForm(forms.ModelForm):
+    class Meta:
+        model = Place
+        fields = ['name', 'location', 'description']
